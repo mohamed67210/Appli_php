f32d430ce80660da2819eec532e3b039a605953d
Mohamed bechri <mohamed-bechri@outlook.fr>
2022-10-06 11:34:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+// : démarrer une session sur le serveur pour l'utilisateur courant
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>ajouter un produit</h1>
+    <form action="traitement.php" method="post">
+        <p>
+            <label>Nom du produit <input type="text" name="nom" /></label>
+        </p>
+        <p>
+            <label>Prix du produit <input type="number" step="any" name="price" /></label>
+        </p>
+        <p>
+            <label>Quantité <input type="number" value="1" name="qtt" /></label>
+        </p>
+        <p>
+            <input type="submit" name="submit" value="Ajouter le produit" />
+        </p>
+    </form>
+</body>
+
+</html>
---
+++ traitement.php
@@ -0,0 +1,8 @@
+<?php 
+if (isset($_POST['submit'])) {
+    $name = filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING);
+    $price = filter_input(INPUT_POST,"price",FILTER_VALIDATE_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
+    $qtt = filter_input(INPUT_POST,"qtt",FILTER_VALIDATE_INT);
+}
+header("Location:index.php");
+?>
