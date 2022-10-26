17f4f9387e9368f89c9cfb0ab7f8910c7a5d8dd8
mohamed67210 <mohamed_bechri@outlook.fr>
2022-10-26 16:56:38 +0200

    modified:   css/style.css 	modified:   index.php 	modified:   product.php 	modified:   recap.php	modified:   traitement.php function findall afficher tt les produits

--- db_functions.php
+++ db_functions.php
@@ -1,23 +1,34 @@
 <?php
  
 // connexion a la base de donnée store
+function connexion(){
     $conn = new \PDO('mysql:host=localhost;dbname=store;charset=utf8', 'root', '',
     [   \PDO::ATTR_ERRMODE  => \PDO::ERRMODE_EXCEPTION,
         \PDO::ATTR_DEFAULT_FETCH_MODE  => \PDO::FETCH_ASSOC,
         \PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES utf8'
     ]);
-
-    function findOneById($conn){
-        $id = (isset($_GET["id"])) ? $_GET["id"] : "";
-        var_dump($id);
+    return $conn;
+}
+    function findAll(){
+        $conn = connexion();
+        $requet = $conn->prepare("SELECT * FROM product");
+        $requet->execute();
+        $products = $requet->fetchAll();
+        return $products;
+    }
+    function findOneById($id){
+        $conn = connexion();
+        // $id = (isset($_GET["id"])) ? $_GET["id"] : "";
+        // var_dump($id);
             $request = $conn->query("SELECT * FROM product WHERE id ='$id' ");
             $request->execute();
-            $products = $request->fetchAll();
-            foreach ($products as $product) {
-                echo '<p>'.$product["name"].'</p>';
-                echo '<p>'.$product["description"].'</p>';
-                echo '<p>'.$product["price"].'</p>';
-            }
+            $product = $request->fetch();
+            return $product;
+    }
+
+    function insertProduct($name, $descr, $price){
+        $conn = connexion();
+        $requete = $conn->prepare("INSERT INTO livre ('name', 'description', 'price') VALUES ($name ,$descr,$price)");
     }
 
 
